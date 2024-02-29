746a939ad97c3745c74334bb55be7caf15379789
Idan-hub2023 <161726206+Idan-hub2023@users.noreply.github.com>
2024-02-29 15:38:12 +0200

    Add files via upload

---
+++ index2.js
@@ -0,0 +1,13 @@
+function hidd(){
+    document.getElementById('books').style.display="none";
+}
+function show(){
+    document.getElementById('books').style.display="flex"
+}
+function noti() {
+    document.getElementById('nn').textContent="pleace creat any accuont";
+}
+
+function check() {
+    window.location.href="login2.html"
+}
